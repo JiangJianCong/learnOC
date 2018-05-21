58913f2ff6986a860663af96a4fa066753c355a0
spacehehe <910455361@qq.com>
2018-05-21 14:17:24 +0800

    带参数的广播发送

--- Notification1/Notification1/AClass.m
+++ Notification1/Notification1/AClass.m
@@ -12,9 +12,13 @@
 -(id)init{
     self = [super init];
     if (self) {
-        [[NSNotificationCenter defaultCenter]postNotificationName:@"TESTNOTIF" object:nil];
-        
-        
+//        [[NSNotificationCenter defaultCenter]postNotificationName:@"TESTNOTIF" object:nil];
+        //发送带参数的广播
+        NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
+        [dic setValue:@"123" forKey:@"keya"];
+        //参数1 ： 广播唯一的名称 参数2 ：发送的唯一对象 参数3 ：字典
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"TESTNOTIF" object:[NSNumber numberWithInt:5] userInfo:dic];
+        //NSNumber 是一个object
     }
     return self;
 }
--- Notification1/Notification1/BClass.m
+++ Notification1/Notification1/BClass.m
@@ -11,13 +11,29 @@
 @implementation BClass
 -(void)testNotification {
     //创建广播
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(testAction) name:@"TESTNOTIF" object:nil];
+    // 参数1:谁在接受这个方法 参数2 ： 响应方法。参数3：广播的名称。参数4:nil
+    // self：当前的这个类 可以主动调用testAction
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(testAction) name:@"TESTNOTIF" object:nil];
+    
+    //带参数的testAction的方法
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(testAction:) name:@"TESTNOTIF" object:nil];
     AClass *classa = [[AClass alloc]init];
     
     
     
 }
 
+/**
+ 声明带参数的广播方法
+
+ @param notif 用来读取数据的
+ */
+-(void) testAction:(NSNotification*)notif {
+    NSNumber *num = notif.object;
+    NSDictionary *dic = notif.userInfo;
+    NSLog(@"%@",num);
+    NSLog(@"%@",dic);
+}
 
 /**
  声明广播的方法
@@ -25,4 +41,12 @@
 -(void) testAction {
     NSLog(@"我已经接受到广播了");
 }
+
+/**
+ 销毁广播
+ */
+-(void) dealloc {
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:@"TESTNOFIT" object:nil];
+}
+
 @end
