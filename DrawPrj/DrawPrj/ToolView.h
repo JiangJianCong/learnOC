f98ff3f1a8b880de6a934364f89b393b1a753ab4
spacehehe <910455361@qq.com>
2018-06-10 16:50:32 +0800

    新建6个UIButton、1个UILabel和1个UISlider并且对其初始化,新建每一个组件的吧block方便外部调用

--- DrawPrj/DrawPrj/ToolView.h
+++ DrawPrj/DrawPrj/ToolView.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ToolView : UIView
+@property (nonatomic, copy) void(^penBlock)(void);
+@property (nonatomic, copy) void(^eraserBlock)();
+@property (nonatomic, copy) void(^colorBlock)();
+@property (nonatomic, copy) void(^undoBlock)();
+@property (nonatomic, copy) void(^clearBlock)();
+@property (nonatomic, copy) void(^saveBlock)();
+@property (nonatomic, copy) void(^sliderBlock)(CGFloat width);
 
 @end
