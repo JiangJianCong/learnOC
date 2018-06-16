b0dfaf4aed4ce061bcea60f4fec2f9f167cac3a4
spacehehe <910455361@qq.com>
2018-06-16 16:28:54 +0800

    视图加载

--- UIViewController/UIViewController/AppDelegate.m
+++ UIViewController/UIViewController/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #define IOS8 [[UIDevice currentDevice].systemVersion doubleValue]>=8
+#import "NewViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,12 +20,48 @@
 // application 启动调用的第一个方法
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    if (IOS8) {
-        // settings 通知的类型 -> 右上角图标
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge categories:nil];
-        // 注册当前的通知
-        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-    }
+//    if (IOS8) {
+//        // settings 通知的类型 -> 右上角图标
+//        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge categories:nil];
+//        // 注册当前的通知
+//        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+//    }
+//
+    
+    // 1 纯代码添加视图控制器
+//    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
+//    self.window.backgroundColor = [UIColor whiteColor];
+//    NewViewController *vc = [[NewViewController alloc]init];
+//
+//    self.window.rootViewController = vc;
+//    [self.window makeKeyAndVisible];
+//
+    
+    /*
+    // 2 纯界面
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    // 根据name着storyboard
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    // 根据storyboard -> vc
+    UIViewController *vc = [storyboard instantiateInitialViewController];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    */
+    // 2 纯界面 根据storyboardId
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    // 根据name着storyboard
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    // 根据storyboard -> vc
+    UIViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"green"];
+    
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    
+    
+    
+    
     
     return YES;
 }
---
+++ UIViewController/UIViewController/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  UIViewController
+//
+//  Created by space on 2018/6/16.
+//  Copyright © 2018 space. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
