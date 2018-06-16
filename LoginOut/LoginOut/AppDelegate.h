d30a0d79aa7af439f5eabe7e616a5a8a4b4d039a
spacehehe <910455361@qq.com>
2018-06-16 16:29:55 +0800

    登陆登出的功能

---
+++ LoginOut/LoginOut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginOut
+//
+//  Created by space on 2018/6/16.
+//  Copyright © 2018 space. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
